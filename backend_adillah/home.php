592a9bb8c585747d2e1d929e95a897e94f57777a
Nacht. <putramadillah@gmail.com>
2024-11-30 14:59:10 +0700

    Delete backend_adillah/home.php

--- backend_adillah/home.php
+++
@@ -1,9 +0,0 @@
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">UTS WEB LANJUT</h1>
-      </div>
-    <h1 class="text-body-emphasis">Identitas Diri</h1>
-    <p class="lead">
-    Nama  : M.Adillah Putra<br>
-    Nim   : 2301083016<br>
-    Kelas : Teknik Komputer 2A     
-  </p>
